02db57fcaa852b3363dbcea2145eed6d34801b4e
Benjamin Gutierrez <kenayguti25@gmail.com>
2025-04-25 19:24:20 -0300

    Update Punto2app.js

--- Punto2app.js
+++ Punto2app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Lista en memoria para los productos
 const productos = [];
 
 app.use(express.urlencoded({ extended: true }));
 
-// Mostrar el formulario
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Agregar producto
 app.post('/agregar', (req, res) => {
     const nombre = req.body.nombre;
     const precio = req.body.precio;
@@ -20,7 +17,6 @@ app.post('/agregar', (req, res) => {
     res.redirect('/listar');
 });
 
-// Mostrar lista de productos
 app.get('/listar', (req, res) => {
     let html = '<h2>Lista de productos</h2><ul>';
     productos.forEach(p => {
@@ -30,7 +26,6 @@ app.get('/listar', (req, res) => {
     res.send(html);
 });
 
-// Iniciar el servidor
 app.listen(3000, () => {
     console.log('Servidor funcionando en http://localhost:3000');
 });
